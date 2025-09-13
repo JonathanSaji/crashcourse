ac49e31d0b744b4f4cf7b5baf0392cff3b0bdfcb
Jonathan Saji <127805037+JonathanSaji@users.noreply.github.com>
2025-09-13 01:10:52 -0400

    first commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/**
+ * The run() method is called automatically when the robot is enabled. If you change the name of
+ * this class or the package after creating this project, you must also update the Main.java file in
+ * the project.
+ */
+public class Robot extends EducationalRobot
+{
+    /**
+     * This constructor is run when the robot is first started up and should be used for any
+     * initialization code.
+     */
+    public void Robot() {}
+    
+    
+    /** This method is run when the robot is enabled. */
+    @Override
+    public void run() {}
+}
